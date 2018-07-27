6a55f9a7d8dd2fbfa31847d630828d3d5bc21633
avrilanne <dunleavyavril@gmail.com>
2018-07-27 09:16:43 -0400

    map working for buses

--- src/components/Buses.js
+++ src/components/Buses.js
@@ -1,18 +1,5 @@
-// import React from 'react'
-
-// const Buses = () => (
-//   <div>
-//     <ul>
-//       <li> Live Bus Positions</li>
-//       <li> Buses by line </li>
-//       <li> Buses by Station </li>
-//     </ul>
-//   </div>
-// )
-
-// export default Buses
-
 import React from 'react';
+import MyMapComponent from './MyMapComponent'
 
 class Buses extends React.Component {
   constructor(props) {
@@ -24,17 +11,27 @@ class Buses extends React.Component {
     };
   }
 
+  formatData=()=> {
+    return this.state.items && this.state.items.map(val => {
+      return (
+        {
+        title:val.VehicleID,
+        key:val.VehicleID,
+        name:val.VehicleID,
+        position:{lat: val.Lat, lng: val.Lon},
+          }
+        )
+    })
+  }
+
   componentDidMount() {
-    // https://api.wmata.com/TrainPositions/TrainPositions?contentType={}&
     fetch("https://api.wmata.com/Bus.svc/json/jBusPositions?api_key=e13626d03d8e4c03ac07f95541b3091b")
-    // fetch("https://api.wmata.com/StationPrediction.svc/json/GetPrediction/A10?api_key=e13626d03d8e4c03ac07f95541b3091b")
-      // .then(results => results)
       .then((response) => {
       return response.json();
       })
       .then((json) => {
         this.setState({
-          items: json,
+          items: json.BusPositions,
           isLoaded: true
         })
       });
@@ -48,9 +45,9 @@ class Buses extends React.Component {
       return <div>Loading...</div>;
     } else {
       return (
-        <pre>
-          {JSON.stringify(this.state.items, null, 2)}
-        </pre>
+        <MyMapComponent
+        data={this.formatData()}
+        />
       );
     }
   }
--- src/components/Trains.js
+++ src/components/Trains.js
@@ -1,17 +1,5 @@
-// import React from 'react'
-
-// const Trains = () => (
-//   <div>
-//     <ul>
-//       <li> Live Train Positions</li>
-//       <li> Trains by line </li>
-//       <li> Train by Station </li>
-//     </ul>
-//   </div>
-// )
-
-
 import React from 'react';
+import MyMapComponent from './MyMapComponent'
 
 class Trains extends React.Component {
   constructor(props) {
@@ -23,16 +11,31 @@ class Trains extends React.Component {
     };
   }
 
+  // Format data ( change bus to train positions )
+
+  formatData=()=> {
+    return this.state.items && this.state.items.map(val => {
+      return (
+        {
+        title:val.TrainId,
+        key:val.TrainId,
+        name:val.TrainId,
+        position:{lat: val.Lat, lng: val.Lon},
+          }
+        )
+    })
+  }
+
   componentDidMount() {
-    // https://api.wmata.com/TrainPositions/TrainPositions?contentType={}&
     fetch("https://api.wmata.com/StationPrediction.svc/json/GetPrediction/A10?api_key=e13626d03d8e4c03ac07f95541b3091b")
       // .then(results => results)
       .then((response) => {
+        console.log(response.json())
       return response.json();
       })
       .then((json) => {
         this.setState({
-          items: json,
+          items: json.TrainPositions,
           isLoaded: true
         })
       });
@@ -46,9 +49,9 @@ class Trains extends React.Component {
       return <div>Loading...</div>;
     } else {
       return (
-        <pre>
-          {JSON.stringify(this.state.items, null, 2)}
-        </pre>
+        <MyMapComponent
+        data={this.formatData()}
+        />
       );
     }
   }
