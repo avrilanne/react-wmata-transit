8a6fc7e5454b0c4d45d371488574ff55213bed5d
avrilanne <dunleavyavril@gmail.com>
2018-07-27 09:19:36 -0400

    map component

---
+++ src/components/MyMapComponent.js
@@ -0,0 +1,39 @@
+import {Map, InfoWindow, Marker, GoogleApiWrapper} from 'google-maps-react';
+import React from 'react';
+
+class MyMapComponent extends React.Component {
+
+
+// spread is ... take all the properies on this object and make it my props
+  getMarkers=()=>{
+    return this.props.data && this.props.data.map(val => {
+      return (
+        <Marker
+        {...val}
+        />
+        )
+    })
+  }
+
+  render(){
+    return(
+      <Map google={this.props.google} zoom={14}>
+
+        {this.getMarkers()}
+
+        <Marker onClick={this.onMarkerClick}
+          name={'Current location'}/>
+        <InfoWindow onClose={this.onInfoWindowClose}>
+          <div>
+            <h1>{console.log(this)}</h1>
+          </div>
+        </InfoWindow>
+      </Map>
+    );
+  }
+}
+
+export default GoogleApiWrapper({
+  apiKey: ("AIzaSyDggNixXJa00YBtnM82MIPdt2vL2fR4nZU")
+})(MyMapComponent)
+
